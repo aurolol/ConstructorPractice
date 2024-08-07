46d87126dd85bc78a8bbdc8dd39ff26ffcaf3d58
auro <auro@DESKTOP-AV8I9BT>
2024-08-07 16:58:58 +0300

    Add project files.

---
+++ ConstructorPractice/Baby.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorPractice
+{
+    public class Baby
+    {
+        public DateTime _birthDate { get; set; }
+        public string _name { get; set; }
+        public string _surName { get; set; }
+
+        public Baby()
+        {
+            Console.WriteLine("Ingaaaaaaaaa!");
+            _birthDate = DateTime.Now;
+        }
+
+        public Baby(string name, string surName)
+        {
+            Console.WriteLine("Ingaaaaaaaaaa!");
+            _birthDate = DateTime.Now;
+            _name = name;
+            _surName = surName;
+            
+        }
+        public void WelcomeWorld()
+        {
+            Console.WriteLine($"Bebeğin adı: {_name} \nSoyadı: {_surName} \nDoğum tarihi: {_birthDate}");
+        }
+    }
+}
---
+++ ConstructorPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ConstructorPractice;
+namespace ConstructorPractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Baby baby1 = new Baby("Atakan", "Çelik");
+
+            baby1.WelcomeWorld();              // Parametre alarak oluşturulan constructor.
+            Console.WriteLine("----------------------------");
+            Baby baby2 = new Baby();
+            baby2._name = "Atakan";
+            baby2._surName = "Çelik";
+            baby2.WelcomeWorld();              //  Default Constructor tarzında parametre almayan bir metot.
+        }
+    }
+}
